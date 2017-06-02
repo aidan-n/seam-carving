ca78d24a6409652883eb5c1584f5f840081ea8c3
aidan-n <aidan-n@users.noreply.github.com>
2017-06-02 01:51:58 -0700

    Update SeamCarver.java

--- SeamCarver.java
+++ SeamCarver.java
@@ -4,7 +4,6 @@ import java.lang.IndexOutOfBoundsException;
 import java.lang.Math;
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import edu.princeton.cs.algs4.Picture;
 
