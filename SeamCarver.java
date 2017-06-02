ea0a792002af86b60327c1d2de2ac454cc0971a3
aidan-n <aidan-n@users.noreply.github.com>
2017-06-01 23:57:06 -0400

    Update SeamCarver.java

--- SeamCarver.java
+++ SeamCarver.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver {
